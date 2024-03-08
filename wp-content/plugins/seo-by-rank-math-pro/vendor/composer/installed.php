7787537f680dd73fb08eaf3403f428b6c8443852
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-08 19:10:03 +0000

    https://github.com/thehcginstitute-com/wp/issues/22

---
+++ wp-content/plugins/seo-by-rank-math-pro/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'rankmath/seo-by-rank-math-pro',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => 'e78b53a7c4287d9b5ddae35b9865aa0c1bd9b45e',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'rankmath/seo-by-rank-math-pro' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => 'e78b53a7c4287d9b5ddae35b9865aa0c1bd9b45e',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
