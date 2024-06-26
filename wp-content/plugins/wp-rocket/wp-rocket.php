ce664936d04584938155b3732a322e7b25bcee42
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-26 18:02:29 +0100

    https://github.com/thehcginstitute-com/wp/issues/31

--- wp-content/plugins/wp-rocket/wp-rocket.php
+++ wp-content/plugins/wp-rocket/wp-rocket.php
@@ -24,7 +24,6 @@ define( 'WP_ROCKET_VERSION',               '3.16.1' );
 define( 'WP_ROCKET_WP_VERSION',            '5.8' );
 define( 'WP_ROCKET_WP_VERSION_TESTED',     '6.3.1' );
 define( 'WP_ROCKET_PHP_VERSION',           '7.3' );
-define( 'WP_ROCKET_PRIVATE_KEY',           false );
 define( 'WP_ROCKET_SLUG',                  'wp_rocket_settings' );
 define( 'WP_ROCKET_WEB_MAIN',              'https://wp-rocket.me/' );
 define( 'WP_ROCKET_WEB_API',               WP_ROCKET_WEB_MAIN . 'api/wp-rocket/' );
